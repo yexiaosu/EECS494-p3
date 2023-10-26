0bde164107d39c162e707a08a5e43b28f4b0fae3
zshahid19 <zshahid19@gmail.com>
2023-10-26 16:31:05 -0400

    -basic player added -basic movement added -basic camera tracking added

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        if (player)
+        {
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+    private bool isJumping = false;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody2D component missing");
+            rb = gameObject.AddComponent<Rigidbody2D>();
+        }
+    }
+
+    void Update()
+    {
+        // Horizontal Movement
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+
+        // Jump Mechanic
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    // Detect collision with ground
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Ground>() != null)
+        {
+            isJumping = false;
+        }
+    }
+}
