324dda9d5ac37bc5d12848785f798e0cc2975c2b
OwenJRoth <rotho@umich.edu>
2023-11-05 23:31:48 -0500

    Stats buff UI

--- Assets/Scripts/Boon.cs
+++ Assets/Scripts/Boon.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.UI;
 
 public class Boon : MonoBehaviour
 {
-    public float speedBoost = 2.0f;  // The additional speed the player gains
-    public float jumpBoost = 5.0f;   // The additional jump force the player gains
+    private float speedBoost = .5f;  // The additional speed the player gains
+    private float jumpBoost = .1f;   // The additional jump force the player gains
+    //public GameObject Canvas;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -13,10 +15,13 @@ public class Boon : MonoBehaviour
         PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
         if (player)
         {
+            BoonText bt = collision.gameObject.GetComponent<BoonText>();
             // Increase player's speed and jump
             player.changeSpeed(player.GetMoveSpeed() + speedBoost);
             player.changeJump(player.GetJumpForce() + jumpBoost);
 
+            bt.getBoon("Speed and Jump");
+            
             // Destroy the boon
             Destroy(gameObject);
         }
