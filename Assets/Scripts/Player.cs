f55cf10c910ab6e4681efdc1e8f16af61444e619
yexiaosu <wang.lan313@sjtu.edu.cn>
2023-11-11 22:55:16 -0500

    attack: jumping on heads as a way to deal damage

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int attack = 500;
+
     public void Dead()
     {
         GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         GetComponent<MessageDisplay>().DisplayMessage("death");
+    }
+
+    public void IncreaseAttack(int damageInc)
+    {
+        attack += damageInc;
     }
 }
