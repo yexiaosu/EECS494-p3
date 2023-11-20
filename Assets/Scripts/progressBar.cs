cff035c636ef9bf955683eca99e320c048d9fbb3
OwenJRoth <rotho@umich.edu>
2023-11-20 00:50:55 -0500

    progress bar

---
+++ Assets/Scripts/progressBar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class progressBar : MonoBehaviour
+{
+    private Transform playerTransform; // Reference to the player's transform
+    public string playerTag = "Player"; // Tag of the player GameObject
+
+    void Start()
+    {
+        // Automatically find the player's transform based on the specified tag
+        GameObject playerObject = GameObject.FindGameObjectWithTag(playerTag);
+
+        if (playerObject != null)
+        {
+            playerTransform = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogError("Player not found! Make sure the player has the specified tag.");
+        }
+    }
+
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            float playerHeight = playerTransform.position.y;
+            playerHeight = playerHeight % 275;
+            playerHeight = playerHeight / 275;
+            playerHeight *= .71f;
+            transform.localScale = new Vector3(.69f, playerHeight, 1);
+        }
+        else
+        {
+            Debug.LogError("Player transform not assigned!");
+        }
+    }
+}
