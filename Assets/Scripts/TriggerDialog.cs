af38040765ca60cbf8928ed4b22ce20cbe1e9943
yexiaosu <wang.lan313@sjtu.edu.cn>
2023-12-03 21:48:59 -0500

    Story is added; story is add to the chain

--- Assets/Scripts/TriggerDialog.cs
+++ Assets/Scripts/TriggerDialog.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class TriggerDialog : MonoBehaviour
 {
+    public GameObject InteractHint;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject.Find("PlayerStory").GetComponent<PlayerSimpleMovement>().AbleToTriggerDialog = true;
+        InteractHint.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         GameObject.Find("PlayerStory").GetComponent<PlayerSimpleMovement>().AbleToTriggerDialog = false;
+        InteractHint.SetActive(false);
     }
 }
